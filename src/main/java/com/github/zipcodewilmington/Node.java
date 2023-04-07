b41c791b657e168403a9cfb159361ee803718c64
dayvidtran <dayvidtran@gmail.com>
2023-04-07 16:10:43 -0400

    start

---
+++ src/main/java/com/github/zipcodewilmington/Node.java
@@ -0,0 +1,29 @@
+package com.github.zipcodewilmington;
+
+public class Node {
+
+    String data;
+
+    Node next;
+
+    public Node(String data){
+        this.data= data;
+        next = null;
+    }
+
+    public String getData(){
+        return data;
+    }
+
+    public void setData(String data){
+        this.data = data;
+    }
+
+    public Node getNext(){
+        return next;
+    }
+
+    public void setNext(Node node){
+        this.next = node;
+    }
+}
--- src/test/java/com/github/zipcodewilmington/DashaMapTest.java
+++ src/test/java/com/github/zipcodewilmington/DashaMapTest.java
@@ -1,9 +1,37 @@
 package com.github.zipcodewilmington;
 
+import org.junit.Test;
+
 /**
  * @author xtofer
  * @version 1.0.0
  * @date 10/21/19 9:05 AM
  */
 public class DashaMapTest {
+    @Test
+    public void name(){
+    }
+
+    @Test
+    public void setTest(){
+    }
+
+    @Test
+    public void deleteTest(){
+
+    }
+    @Test
+    public void getTest(){
+
+    }
+    @Test
+    public void isEmptyTest(){
+
+    }
+
+    @Test
+    public void sizeTest(){
+
+    }
 }
+
